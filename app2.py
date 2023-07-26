2c2a5445f9f800521fc83a742e3ae427d66e2db0
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2023-07-26 12:28:50 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -516,7 +516,7 @@ def Quantification():
         col3,col4=st.sidebar.columns(2)
         col3.image("https://ilm.univ-lyon1.fr/templates/mojito/images/logo.jpg", use_column_width=True)
         col4.image("https://formation-professionnelle.universite-lyon.fr/var/site/storage/images/3/3/5/0/533-17-fre-FR/Lyon-1-Claude-Bernard.png", use_column_width=True)
-       st.sidebar.write("<p style='text-align: center;'>Alioune Gaye : Stagiaire ILM (%s)</p>" % url, unsafe_allow_html=True)
+        st.sidebar.write("<p style='text-align: center;'>Alioune Gaye : Stagiaire ILM (%s)</p>" % url, unsafe_allow_html=True)
         st.sidebar.write("<p style='text-align: center;'> Encadrent:Martini Matéo </p>", unsafe_allow_html=True)
         st.sidebar.write("<p style='text-align: center;'>  Les methodes utilisées sont:  </p>", unsafe_allow_html=True)
         st.sidebar.markdown("<p style='text-align: center;'>1 - Méthode mono_exponentielle : </p>", unsafe_allow_html=True)
