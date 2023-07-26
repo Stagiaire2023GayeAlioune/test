153c7b991f0e2084f6eb24535346690de4933979
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2023-07-26 11:06:28 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -181,7 +181,7 @@ def identification():
             def predict_quality(model, df):
                   predictions_data = predict_model(estimator = model, data = df)
                   return predictions_data
-            model =load_model('best_model.pkl')
+            model =load_model('best_class_model.pkl')
             pred=predict_quality(model, df_to_predict)
             st.dataframe(pred[pred.columns[-3:]].head())
         else:
