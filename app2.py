c270bdc3caa200ead90e0eb82e50470f2c735798
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2023-08-29 21:25:47 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -712,8 +712,6 @@ def Quantification():
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt;
-from tkinter import filedialog
-from tkinter import *
 from scipy.signal import find_peaks
 from scipy.signal import savgol_filter
 from scipy.optimize import curve_fit;
