3059671eed1163dbaa00561b2b0cb77f20e24603
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2023-10-29 19:40:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ def main():
     col3,col4=st.sidebar.columns(2)
     #st.sidebar.write("<p style='text-align: center;'> Sokhna Faty Bousso : Stagiaire ILM (%s)</p>" % url, unsafe_allow_html=True)
     st.sidebar.write("<p style='text-align: center;'>Automatisation de l'emploi du temps de ma princesse</p>", unsafe_allow_html=True)
-    methode1=st.selectbox("selectionner l'heure que tu termine les cours demain",("Week-end","8","9","10","11","12","13","14","15","16","17","18"))
+    methode1=st.selectbox("selectionner l'heure que tu termine les cours demain",("Weekend","8","9","10","11","12","13","14","15","16","17","18"))
     methode2=st.selectbox("Est ce que tu iras au boulot demain?",("Oui","Non"))
-    if methode1=="Week-end" and methode2=="Non":
+    if methode1=="Weekend" and methode2=="Non":
         st.write("Demain essaye de te reveiller le plus tot possible pour apprendre les cours, TD ou Tp du lundi si possible du mardi aussi ") 
-    if methode1=="Week-end" and methode2=="Oui":
+    if methode1=="Weekend" and methode2=="Oui":
             st.write("Demain, Tu pourras te reposer à condition que tu travail le matin et le soir")
     if methode1=="18":
         st.write("Ma princesse, demain tu rentre à la maison aprés les cours")     
